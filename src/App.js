1a2641f919c38d6db211060016620230838ab30b
Nhat Quang <thaihuynhatquang@gmail.com>
2019-01-24 11:54:08 +0700

    fetcher

--- src/App.js
+++ src/App.js
@@ -1,48 +1,87 @@
 import React from 'react'
-import { compose, withStateHandlers } from 'recompose'
+import { compose, withStateHandlers, lifecycle, withHandlers, mapProps } from 'recompose'
+import { upperFirst, omit } from 'lodash'
 
-const ToggleHelloWorld = props => {
-  const {
-    isWorldShow,
-    showWorld,
-    hideWorld,
-    toggleWorld,
-    isTooltipShow,
-    showTooltip,
-    hideTooltip
-  } = props
+const withFetcher = (name, fetch, fetchOnMount = false) => compose(
+  withStateHandlers(
+    {
+      [`${name}Fetcher`]: {
+        data: null,
+        loading: false,
+        error: null
+      }
+    },
+    {
+      [`receive${upperFirst(name)}Data`]: () => (data: any) => ({
+        [`${name}Fetcher`]: {
+          data,
+          loading: false,
+          error: null
+        }        
+      }),
+      [`receive${upperFirst(name)}Error`]: ({
+        [`${name}Fetcher`]: { data }
+      }) => (error: any) => ({
+        [`${name}Fetcher`]: {
+          data,
+          loading: false,
+          error: error || true
+        }      
+      }),
+      [`start${upperFirst(name)}Fetch`]: ({
+        [`${name}Fetcher`]: prevState
+      }) => () => ({
+        [`${name}Fetcher`]: {
+          ...prevState,
+          loading: true
+        }
+      })
+    }
+  ),
+  withHandlers({
+    [`fetch${upperFirst(name)}`]: (props: any) => () => {
+      props[`start${upperFirst(name)}Fetch`]()
+      fetch(props).then(
+        props[`receive${upperFirst(name)}Data`],
+        props[`receive${upperFirst(name)}Error`]
+      )
+    }
+  }),
+  mapProps(props =>
+    omit(props, [
+      `receive${upperFirst(name)}Data`,
+      `receive${upperFirst(name)}Error`,
+      `start${upperFirst(name)}Fetch`
+    ])
+  ),
+  lifecycle({
+    componentDidMount() {
+      this.props["fetch" + upperFirst(name)]()
+    }
+  })
+)
 
+const Test = props => {
+  const isLoading = (props.testFetcher.loading === true) ? 
+    "Loading..." : 
+    "hello"
+  console.log(props)
   return <div>
-    {isWorldShow && 'Hello world'}
-    <button onClick={showWorld}>Show</button>
-    <button onClick={hideWorld}>Hide</button>
-    <button onClick={toggleWorld}>Toggle</button>
-
-    <div onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>Hover me!</div>
-    {isTooltipShow && <div>Yay!</div>}
+    { isLoading }
   </div>
 }
 
-const withToggle = Name => compose(
-  withStateHandlers(
-    {
-      [`is${Name}Show`]: false
-    },
-    {
-      [`show${Name}`]: () => () => ({
-        [`is${Name}Show`]: true
-      }),
-      [`hide${Name}`]: () => () => ({
-        [`is${Name}Show`]: false
-      }),
-      [`toggle${Name}`]: state => () => ({
-        [`is${Name}Show`]: !state[`is${Name}Show`]
-      })
-    }
-  )
-)
-
 export default compose(
-  withToggle('World'),
-  withToggle('Tooltip')
-)(ToggleHelloWorld);
+  withFetcher(
+    "test",
+    async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+        return await response.json();
+      } catch (error) {
+        throw error.message;
+      }
+    },
+    { fetchOnMount: true }
+  )
+)(Test);
